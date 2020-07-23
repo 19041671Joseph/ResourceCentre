85304142736484ef58df51baed1d41f70f45ddf1
19041265 <19041265@DESKTOP-TUA3BPF>
2020-07-23 14:58:03 +0800

    Added the missing stuff and I located an error for retrieveAllChromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -147,7 +147,7 @@ public class ResourceCentre {
 		String output = "";
 		for (int i = 0; i < chromebookList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", chromebookList.get(i).getAssetTag(),
+			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
 					chromebookList.get(i).getDescription(), 
 					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
 					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
@@ -156,7 +156,10 @@ public class ResourceCentre {
 		return output;
 	}
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
-		
+		ResourceCentre.setHeader("CHROMEBOOK");
+		String display = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ASSET TAG", "DESCRIPTION",
+				"AVAILABLE", "DUE DATE","OPERATING SYSTEM");
+		System.out.println(display);
 		String output = retrieveAllChromebook(chromebookList);
 		System.out.println(output);
 	}
